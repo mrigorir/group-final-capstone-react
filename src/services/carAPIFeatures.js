91dfc71a9d0b0934d29d0b3db0d9df9b8a62f57c
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-10-27 23:32:13 +0100

    update

--- src/services/carAPIFeatures.js
+++ src/services/carAPIFeatures.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const id = 19;
-const url = 'https://morning-chamber-68237.herokuapp.com/api/v1/cars/20';
+const url = 'https://morning-chamber-68237.herokuapp.com/api/v1/cars/21';
 
 const getCars = async () => {
   const response = await axios.get(url);
