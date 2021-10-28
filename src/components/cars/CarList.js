0ddabdd092129b1d09ac12da45fac2815c46616a
Marco <marcoparra311@gmail.com>
2021-10-27 23:34:01 -0400

    Create Show and Remove cars features

--- src/components/cars/CarList.js
+++ src/components/cars/CarList.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import hooks from '../../redux/hooks/hooks';
 import Car from './Car';
 
-const CarList = () => (
-  <div className="position-relative">
-    <div className="carList-pos">
-      <h1 className="text-white text-center p-3 bg-green mx-auto mb-5 carListTitle"> Your list of cars </h1>
-      <ul className="list-unstyled fs-4 fw-bold text-green shadow-lg border-green border-2 mx-auto rounded mb-5 carList p-3">
-        <Car />
-      </ul>
+const CarList = () => {
+  const addDeleteCars = useSelector((state) => state.addDeleteCarsReducer);
+  hooks();
+
+  return (
+    <div className="position-relative">
+      <div className="carList-pos">
+        <h1 className="text-white text-center p-3 bg-green mx-auto mb-5 carListTitle"> Your list of cars </h1>
+        <ul className="list-unstyled fs-4 fw-bold text-green shadow-lg border-green border-2 mx-auto rounded mb-5 carList p-3">
+          {addDeleteCars.map((show) => (
+            <Car key={show.id} id={show.id} name={show.name} image={show.image} />
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CarList;
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import carsReducer from './cars/cars';
+import { addDeleteCarsReducer } from './cars/add&deleteCars';
 
 const reducer = combineReducers({
   carsReducer,
+  addDeleteCarsReducer,
 });
 
 const store = createStore(
